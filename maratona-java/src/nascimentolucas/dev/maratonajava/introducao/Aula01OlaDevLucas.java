70f62123e9b8d9580619da42660f8a2f8688d072
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-01-29 16:34:58 -0300

    feat: aula 02 - tipos primitivos

--- maratona-java/src/nascimentolucas/dev/maratonajava/introducao/Aula01OlaDevLucas.java
+++ maratona-java/src/nascimentolucas/dev/maratonajava/introducao/Aula01OlaDevLucas.java
@@ -1,6 +1,6 @@
 package nascimentolucas.dev.maratonajava.introducao;
 
-public class OlaDevLucas {
+public class Aula01OlaDevLucas {
     public static  void main(String[] args){
         System.out.println("Hello World");
     }
