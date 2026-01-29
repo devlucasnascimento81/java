de513883434c45e49afe2a898d4093953f03b8d7
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-01-28 22:38:40 -0300

    chore:preparando setup inicial

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/introducao/OlaDevLucas.java
@@ -0,0 +1,7 @@
+package nascimentolucas.dev.maratonajava.introducao;
+
+public class OlaDevLucas {
+    public static  void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
