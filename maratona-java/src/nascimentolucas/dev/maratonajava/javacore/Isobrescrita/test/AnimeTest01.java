288d3c873b04a66854838acf9ca7eba71123c0b9
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-02-26 16:02:54 -0300

    feat: aula 17 - orientação objetos: sobrescrita de método

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package nascimentolucas.dev.maratonajava.javacore.Isobrescrita.test;
+
+import nascimentolucas.dev.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Blood blockgrade battlefront");
+        System.out.println(anime);
+    }
+}
