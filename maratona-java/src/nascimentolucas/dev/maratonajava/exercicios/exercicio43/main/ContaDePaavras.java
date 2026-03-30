79ed1329af5b3a5f20558b6b29143c9c84768e55
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-03-30 16:30:07 -0300

    feat: resolve exercício 43 - contador de palavras

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/exercicios/exercicio43/main/ContaDePaavras.java
@@ -0,0 +1,20 @@
+package nascimentolucas.dev.maratonajava.exercicios.exercicio43.main;
+
+public class ContaDePaavras {
+
+    public static int contarPalavra(String[] palavras, String buscada) {
+        int contador = 0;
+        for (String p : palavras) {
+            if (p.equalsIgnoreCase(buscada)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static void main(String[] args) {
+        String[] palavras = {"java","python","javali","dominó","java","relogio","gelo","java"};
+        int resultado = contarPalavra(palavras,"java");
+        System.out.println(resultado);
+    }
+}
