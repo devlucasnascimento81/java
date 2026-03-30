26b652d3ab7cf84990e48bc0bc248fdf9a61f821
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-03-30 17:31:54 -0300

    feat: resolve exercício 44 - sequência fibonacci

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/exercicios/exercicio44/main/SequenciaFibonacci.java
@@ -0,0 +1,19 @@
+package nascimentolucas.dev.maratonajava.exercicios.exercicio44.main;
+
+public class SequenciaFibonacci {
+    public static void fibonacci(int n) {
+        int anterior = 0;
+        int atual = 1;
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(anterior);
+            int proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+        }
+    }
+
+    public static void main(String[] args) {
+        fibonacci(10);
+    }
+}
