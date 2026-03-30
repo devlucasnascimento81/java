be55fdeeb424a56800ffdce349e35ef5f5c95ac0
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-03-30 15:57:48 -0300

    feat: resolve exercício 42 - verificador de palídromo

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/exercicios/exercicio42/main/Palidromo.java
@@ -0,0 +1,26 @@
+package nascimentolucas.dev.maratonajava.exercicios.exercicio42.main;
+
+public class Palidromo {
+    public static boolean isPalindromo(String palavra) {
+       int inicio = 0;
+       int fim = palavra.length() - 1;
+
+       while (inicio < fim) {
+
+           if (palavra.charAt(inicio) != palavra.charAt(fim)) {
+               return false;
+           }
+
+           inicio++;
+           fim--;
+       }
+       return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindromo("arara"));
+        System.out.println(isPalindromo("java"));
+        System.out.println(isPalindromo("radar"));
+
+    }
+}
