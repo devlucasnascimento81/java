09c2afa2cebe5a78cb3f81cd9a17d0234fe1f9b7
devlucasnascimento81 <dev.lucasnascimento081@gmail.com>
2026-03-31 16:23:49 -0300

    feat: resolve exercício 45 - jokenpô game

---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/exercicios/exercicio45/main/Jogada.java
@@ -0,0 +1,7 @@
+package nascimentolucas.dev.maratonajava.exercicios.exercicio45.main;
+
+public enum Jogada {
+    PEDRA,
+    PAPEL,
+    TESOURA
+}
---
+++ maratona-java/src/nascimentolucas/dev/maratonajava/exercicios/exercicio45/main/Jokepo.java
@@ -0,0 +1,58 @@
+package nascimentolucas.dev.maratonajava.exercicios.exercicio45.main;
+
+import java.util.Random;
+
+public class Jokepo {
+    public static String verficarVencedor(Jogada jogador, Jogada computador) {
+        switch (jogador) {
+            case PEDRA -> {
+                if (computador == Jogada.PAPEL) return "computador vence!";
+                if (computador == Jogada.TESOURA) return "jogador vence!";
+                return "empate";
+            }
+            case PAPEL -> {
+                if (computador == Jogada.TESOURA) return "computador vence!";
+                if (computador == Jogada.PEDRA) return "jogador vence";
+                return "empate";
+            }
+            case TESOURA -> {
+                if (computador == Jogada.PEDRA) return "computador vence!";
+                if (computador == Jogada.PAPEL) return "jogador vence";
+                return "empate";
+            }
+            default -> {
+                return "Jogada inválida";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Jogada[] jogadas = Jogada.values();
+
+        int pontosJogador = 0;
+        int pontosComputador = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("----- Rodada " + i + " -----");
+            Jogada computador = jogadas[new Random().nextInt(jogadas.length)];
+            System.out.println("Computador: " + computador);
+            Jogada jogador = Jogada.PEDRA;
+            System.out.println("Jogador: " + jogador);
+            String resultado = verficarVencedor(jogador, computador);
+            System.out.println(resultado);
+            if (resultado.equals("jogador vence!")) {
+                pontosJogador++;
+            } else if (resultado.equals("computador vence!")) {
+                pontosComputador++;
+            }
+        }
+
+        System.out.println("------ PLACAR ------");
+        System.out.println("Pontos jogador: " + pontosJogador);
+        System.out.println("Pontos computador: " + pontosComputador);
+
+
+    }
+
+
+}
